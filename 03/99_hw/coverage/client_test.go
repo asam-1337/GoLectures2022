a6eb9963979c663975bc04411479b4a9430eb5a9
asam-1337 <samenkov0280@gmail.com>
2022-09-02 13:59:50 +0300

    asam-1337 HomeWork 3

--- 03/99_hw/coverage/client_test.go
+++ 03/99_hw/coverage/client_test.go
@@ -1,3 +1,40 @@
 package main
 
 // тут писать код тестов
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// тут писать код тестов
+
+func TestSearchClient_FindUsers(t *testing.T) {
+	cases := []SearchRequest{
+		{
+			Limit:      20,
+			Query:      "commodo",
+			OrderField: "Name",
+			OrderBy:    OrderByAsIs,
+		},
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	for caseNum, item := range cases {
+		sc := &SearchClient{
+			AccessToken: "asdas",
+			URL:         ts.URL,
+		}
+		result, err := sc.FindUsers(item)
+		if err != nil {
+			fmt.Println(caseNum, ": FUCK , error: ", err)
+		}
+		if err == nil {
+			fmt.Println(result.Users)
+		}
+	}
+	ts.Close()
+
+}
